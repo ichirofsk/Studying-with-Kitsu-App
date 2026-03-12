981c499bb3e15f530f17a263a6ff4c76fb970832
ichirofsk <evandroichirofusesakai@gmail.com>
2026-03-12 16:03:41 -0300

    Initial Commit

---
+++ Studying with Kitsu App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Studying with Kitsu App
+//
+//  Created by Evandro Ichiro Fuse Sakai on 12/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Studying with Kitsu App/Studying_with_Kitsu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Studying_with_Kitsu_AppApp.swift
+//  Studying with Kitsu App
+//
+//  Created by Evandro Ichiro Fuse Sakai on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Studying_with_Kitsu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
